c007fe0853c2f2e35b3afb024da8d9c317a57439
Mahmoud Hisham <mahmoudhisham11@gmail.com>
2025-12-10 13:50:25 +0200

    Add new files and updates

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,15 @@
-import { Geist, Geist_Mono } from "next/font/google";
+// src/app/layout.js
+import { Inter, Oswald } from "next/font/google";
 import "./globals.css";
 import { ProductsProvider } from "./context/ProductsContext";
 
-const geistSans = Geist({
+// استخدام Google Fonts الصحيحة
+const geistSans = Inter({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
+const geistMono = Oswald({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
@@ -20,20 +22,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Playwrite+NO:wght@100..400&display=swap"
-          rel="stylesheet"
-        />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&family=Playwrite+NO:wght@100..400&display=swap"
-          rel="stylesheet"
-        />
-      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ProductsProvider>{children}</ProductsProvider>
       </body>
